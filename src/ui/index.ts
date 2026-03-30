8c968b3112ad904eb76d445eed1e3bc4e386e770
JustinClarke <justinclarke241@gmail.com>
2026-03-30 17:51:36 +0000

    feat: add ui index exports

---
+++ src/ui/index.ts
@@ -0,0 +1,12 @@
+export * from './Badge';
+export * from './ThemeToggle';
+export * from './MagneticButton';
+export * from './SpotlightCard';
+
+export * from './ErrorBoundary';
+export * from './ScrollReveal';
+export * from './SectionContainer';
+export * from './SkeletonLoader';
+export * from './BackToTerminal';
+export * from './FireParticles';
+export * from './TechStack';
